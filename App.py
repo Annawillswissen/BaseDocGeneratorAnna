afc7e48076c607761e39f0695ec5f5986089cb1d
imoDDomi <dominik.polo98@googlemail.com>
2024-05-06 13:15:24 +0200

    v1 test

--- App.py
+++ App.py
@@ -1,4 +1,5 @@
-import sys
+import sys, os, docx
+from docxcompose.composer import Composer
 from PySide6.QtWidgets import QApplication, QMainWindow
 from UI.Mainwindow.Mainwindow import Ui_MainWindow  # Pfad entsprechend anpassen
 
@@ -8,27 +9,77 @@ class MainWindow(QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         
-        # Verbinde den Button-Klick mit der on_pb_test_clicked Funktion
-        self.ui.pb_Schnittstellen.clicked.connect(self.switch_to_Interlocks)          #_test.clicked.connect(self.on_pb_test_clicked)
-        self.ui.pb_SoO.clicked.connect(self.switch_to_SoO)
-        self.ui.pb_spare.clicked.connect(self.switch_to_spare)
+        # Navigation Buttons
+        self.ui.pb_Schnittstellen.clicked.connect(lambda: self.switch_widget(0))          #_test.clicked.connect(self.on_pb_test_clicked)
+        self.ui.pb_SoO.clicked.connect(lambda: self.switch_widget(1))
+        self.ui.pb_spare.clicked.connect(lambda: self.switch_widget(2))
+
+        # start generating documents
+        self.ui.pb_start.clicked.connect(self.generator)
 
 
 
 
 
 
-    def switch_to_Interlocks(self):
-        self.ui.sw_test.setCurrentIndex(0)
+    def switch_widget(self,index):
+        self.ui.sw_nav_bar.setCurrentIndex(index)
 
-    def switch_to_SoO(self):
-        self.ui.sw_test.setCurrentIndex(1)
+    
+    def generator(self):
 
-    def switch_to_spare(self):
-        self.ui.sw_test.setCurrentIndex(2)
+        # Basispfad zu den Vorlagen
+        base_path = "Z:/vm_austausch/SS_Tool/vorlagenpool"
         
-        
+        # erzeuge Pfad für Anlagentyp
+        path = os.path.join(base_path, self.ui.cob_typ.currentData)
 
+        # erzeuge Basis Dokument
+        basedoc = docx.Document(os.path.join(path, "1_head", "head.docx"))
+
+        # restliche Dokumente zum zusammenführen
+        snippet_list = []
+
+        # Hardware
+        snippet_list.append(os.path.join(path, "2_hardware", self.ui.cob_hardware.currentData, self.ui.cob_hardware.currentData + "_" + self.ui.cob_interlock_length.currentData + ".docx"))
+        
+        # Wenn Option 24-poliger Stecker, füge Seite ein
+        if self.ui.cb_24_pins.isChecked == True:
+            snippet_list.append(os.path.join(path, "2_hardware", "optional_24pol", "optional_24pol.docx"))
+        
+        # Safety Signals
+        if self.ui.cb_24_pins.isChecked == True:
+            snippet_list.append(os.path.join(path, "3_safety_signals", "safety_24pol", "safety_24pol.docx"))
+        
+        else:
+            snippet_list.append(os.path.join(path, "3_safety_signals", "profisafe.docx"))
+
+        # Standard Signals
+        snippet_list.append(os.path.join(path, "4_standard_signals", "signals.docx"))
+
+        # End 
+        snippet_list.append(os.path.join(path, "5_end", "end.docx"))
+    
+        # erzeuge Composer Objekt
+        composer = Composer(basedoc)
+
+        # erzeuge Seitenumbruch nach erster Seite
+        basedoc.add_page_break()
+
+        # füge alle restlichen snippets hinzu
+        for i, snippet in enumerate(snippet_list):
+            tmp_object = docx.Document(snippet)
+            
+            # Bei letztem Snippet keinen Pagebreak mehr
+            if i < len(snippet_list)-1:
+                tmp_object.add_page_break()
+
+            composer.append(tmp_object)
+            
+            
+        # neues Dokument speichern
+        
+        composer.save('Z:/vm_austausch/merged_document.docx')
 
 
 
--- UI/Mainwindow/Mainwindow.py
+++ UI/Mainwindow/Mainwindow.py
@@ -15,51 +15,214 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QMainWindow, QPushButton, QSizePolicy,
-    QStackedWidget, QTextEdit, QWidget)
+from PySide6.QtWidgets import (QApplication, QCheckBox, QComboBox, QLabel,
+    QLineEdit, QMainWindow, QPushButton, QSizePolicy,
+    QStackedWidget, QWidget)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(772, 541)
+        MainWindow.resize(800, 500)
+        MainWindow.setMinimumSize(QSize(800, 500))
+        MainWindow.setMaximumSize(QSize(800, 500))
+        MainWindow.setStyleSheet(u"")
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
         self.pb_Schnittstellen = QPushButton(self.centralwidget)
         self.pb_Schnittstellen.setObjectName(u"pb_Schnittstellen")
-        self.pb_Schnittstellen.setGeometry(QRect(40, 190, 91, 24))
+        self.pb_Schnittstellen.setGeometry(QRect(30, 100, 131, 41))
+        self.pb_Schnittstellen.setStyleSheet(u"QPushButton {\n"
+"	color: rgb(255, 255, 255);\n"
+"	background-color: rgb(45, 45, 45);\n"
+"	border-radius: 10px;\n"
+"}\n"
+"QPushButton:hover {\n"
+"	color: rgb(255,255,255);\n"
+"	background-color: rgb(68, 68, 68);\n"
+"	border-radius: 10px;\n"
+"}\n"
+"QPushButton:pressed {\n"
+"	background-color: rgb(0, 0, 0);\n"
+"	color: rgb(255, 255, 255);\n"
+"	border-radius: 10px;\n"
+"}")
         self.pb_SoO = QPushButton(self.centralwidget)
         self.pb_SoO.setObjectName(u"pb_SoO")
-        self.pb_SoO.setGeometry(QRect(40, 220, 75, 24))
+        self.pb_SoO.setGeometry(QRect(30, 150, 131, 41))
+        self.pb_SoO.setStyleSheet(u"QPushButton {\n"
+"	color: rgb(255, 255, 255);\n"
+"	background-color: rgb(45, 45, 45);\n"
+"	border-radius: 10px;\n"
+"}\n"
+"QPushButton:hover {\n"
+"	color: rgb(255,255,255);\n"
+"	background-color: rgb(68, 68, 68);\n"
+"	border-radius: 10px;\n"
+"}\n"
+"QPushButton:pressed {\n"
+"	background-color: rgb(0, 0, 0);\n"
+"	color: rgb(255, 255, 255);\n"
+"	border-radius: 10px;\n"
+"}")
         self.pb_spare = QPushButton(self.centralwidget)
         self.pb_spare.setObjectName(u"pb_spare")
-        self.pb_spare.setGeometry(QRect(40, 250, 75, 24))
-        self.sw_test = QStackedWidget(self.centralwidget)
-        self.sw_test.setObjectName(u"sw_test")
-        self.sw_test.setGeometry(QRect(390, 130, 301, 261))
-        self.page = QWidget()
-        self.page.setObjectName(u"page")
-        self.textEdit = QTextEdit(self.page)
-        self.textEdit.setObjectName(u"textEdit")
-        self.textEdit.setGeometry(QRect(80, 50, 131, 41))
-        self.sw_test.addWidget(self.page)
-        self.page_2 = QWidget()
-        self.page_2.setObjectName(u"page_2")
-        self.textEdit_2 = QTextEdit(self.page_2)
-        self.textEdit_2.setObjectName(u"textEdit_2")
-        self.textEdit_2.setGeometry(QRect(90, 60, 131, 41))
-        self.sw_test.addWidget(self.page_2)
-        self.page_3 = QWidget()
-        self.page_3.setObjectName(u"page_3")
-        self.textEdit_3 = QTextEdit(self.page_3)
-        self.textEdit_3.setObjectName(u"textEdit_3")
-        self.textEdit_3.setGeometry(QRect(160, 100, 131, 41))
-        self.sw_test.addWidget(self.page_3)
+        self.pb_spare.setGeometry(QRect(30, 200, 121, 41))
+        self.pb_spare.setStyleSheet(u"QPushButton {\n"
+"	color: rgb(255, 255, 255);\n"
+"	background-color: rgb(45, 45, 45);\n"
+"	border-radius: 10px;\n"
+"}\n"
+"QPushButton:hover {\n"
+"	color: rgb(255,255,255);\n"
+"	background-color: rgb(68, 68, 68);\n"
+"	border-radius: 10px;\n"
+"}\n"
+"QPushButton:pressed {\n"
+"	background-color: rgb(0, 0, 0);\n"
+"	color: rgb(255, 255, 255);\n"
+"	border-radius: 10px;\n"
+"}")
+        self.widget = QWidget(self.centralwidget)
+        self.widget.setObjectName(u"widget")
+        self.widget.setGeometry(QRect(10, 10, 191, 481))
+        self.widget.setStyleSheet(u"background-color: rgb(35, 35, 35);\n"
+"border-radius: 25px;")
+        self.label = QLabel(self.widget)
+        self.label.setObjectName(u"label")
+        self.label.setGeometry(QRect(0, 400, 191, 41))
+        font = QFont()
+        font.setPointSize(15)
+        font.setBold(True)
+        self.label.setFont(font)
+        self.label.setStyleSheet(u"background-color: rgb(255, 255, 0);")
+        self.sw_nav_bar = QStackedWidget(self.centralwidget)
+        self.sw_nav_bar.setObjectName(u"sw_nav_bar")
+        self.sw_nav_bar.setGeometry(QRect(220, 10, 571, 481))
+        self.Interlocks = QWidget()
+        self.Interlocks.setObjectName(u"Interlocks")
+        self.label1 = QLabel(self.Interlocks)
+        self.label1.setObjectName(u"label1")
+        self.label1.setGeometry(QRect(10, 70, 49, 16))
+        font1 = QFont()
+        font1.setBold(True)
+        self.label1.setFont(font1)
+        self.label_2 = QLabel(self.Interlocks)
+        self.label_2.setObjectName(u"label_2")
+        self.label_2.setGeometry(QRect(10, 100, 49, 16))
+        self.label_2.setFont(font1)
+        self.label_3 = QLabel(self.Interlocks)
+        self.label_3.setObjectName(u"label_3")
+        self.label_3.setGeometry(QRect(10, 130, 49, 16))
+        self.label_3.setFont(font1)
+        self.label_4 = QLabel(self.Interlocks)
+        self.label_4.setObjectName(u"label_4")
+        self.label_4.setGeometry(QRect(10, 190, 49, 16))
+        self.label_4.setFont(font1)
+        self.label_5 = QLabel(self.Interlocks)
+        self.label_5.setObjectName(u"label_5")
+        self.label_5.setGeometry(QRect(10, 220, 61, 16))
+        self.label_5.setFont(font1)
+        self.label_6 = QLabel(self.Interlocks)
+        self.label_6.setObjectName(u"label_6")
+        self.label_6.setGeometry(QRect(10, 280, 141, 16))
+        self.label_6.setFont(font1)
+        self.le_fn_number = QLineEdit(self.Interlocks)
+        self.le_fn_number.setObjectName(u"le_fn_number")
+        self.le_fn_number.setGeometry(QRect(150, 70, 113, 22))
+        self.le_customer = QLineEdit(self.Interlocks)
+        self.le_customer.setObjectName(u"le_customer")
+        self.le_customer.setGeometry(QRect(150, 100, 201, 22))
+        self.le_firstname = QLineEdit(self.Interlocks)
+        self.le_firstname.setObjectName(u"le_firstname")
+        self.le_firstname.setGeometry(QRect(150, 130, 201, 22))
+        self.le_lastname = QLineEdit(self.Interlocks)
+        self.le_lastname.setObjectName(u"le_lastname")
+        self.le_lastname.setGeometry(QRect(360, 130, 181, 22))
+        self.cob_typ = QComboBox(self.Interlocks)
+        self.cob_typ.addItem("")
+        self.cob_typ.addItem("")
+        self.cob_typ.addItem("")
+        self.cob_typ.setObjectName(u"cob_typ")
+        self.cob_typ.setGeometry(QRect(150, 190, 91, 22))
+        self.cob_hardware = QComboBox(self.Interlocks)
+        self.cob_hardware.addItem("")
+        self.cob_hardware.addItem("")
+        self.cob_hardware.addItem("")
+        self.cob_hardware.setObjectName(u"cob_hardware")
+        self.cob_hardware.setGeometry(QRect(150, 220, 91, 22))
+        self.label_11 = QLabel(self.Interlocks)
+        self.label_11.setObjectName(u"label_11")
+        self.label_11.setGeometry(QRect(10, 250, 91, 16))
+        self.label_11.setFont(font1)
+        self.cob_interlock_length = QComboBox(self.Interlocks)
+        self.cob_interlock_length.addItem("")
+        self.cob_interlock_length.addItem("")
+        self.cob_interlock_length.addItem("")
+        self.cob_interlock_length.setObjectName(u"cob_interlock_length")
+        self.cob_interlock_length.setGeometry(QRect(150, 250, 69, 22))
+        self.cb_24_pins = QCheckBox(self.Interlocks)
+        self.cb_24_pins.setObjectName(u"cb_24_pins")
+        self.cb_24_pins.setGeometry(QRect(170, 280, 21, 20))
+        self.pb_start = QPushButton(self.Interlocks)
+        self.pb_start.setObjectName(u"pb_start")
+        self.pb_start.setGeometry(QRect(10, 350, 111, 51))
+        self.label_10 = QLabel(self.Interlocks)
+        self.label_10.setObjectName(u"label_10")
+        self.label_10.setGeometry(QRect(10, 310, 61, 16))
+        self.label_10.setFont(font1)
+        self.cob_language = QComboBox(self.Interlocks)
+        self.cob_language.addItem("")
+        self.cob_language.setObjectName(u"cob_language")
+        self.cob_language.setGeometry(QRect(150, 310, 69, 22))
+        self.le_email = QLineEdit(self.Interlocks)
+        self.le_email.setObjectName(u"le_email")
+        self.le_email.setGeometry(QRect(150, 160, 391, 22))
+        self.label_15 = QLabel(self.Interlocks)
+        self.label_15.setObjectName(u"label_15")
+        self.label_15.setGeometry(QRect(50, 10, 430, 50))
+        font2 = QFont()
+        font2.setPointSize(25)
+        font2.setBold(True)
+        self.label_15.setFont(font2)
+        self.label_15.setAlignment(Qt.AlignmentFlag.AlignLeading|Qt.AlignmentFlag.AlignLeft|Qt.AlignmentFlag.AlignTop)
+        self.sw_nav_bar.addWidget(self.Interlocks)
+        self.SoO = QWidget()
+        self.SoO.setObjectName(u"SoO")
+        self.label_12 = QLabel(self.SoO)
+        self.label_12.setObjectName(u"label_12")
+        self.label_12.setGeometry(QRect(100, 150, 341, 161))
+        font3 = QFont()
+        font3.setPointSize(20)
+        self.label_12.setFont(font3)
+        self.label_16 = QLabel(self.SoO)
+        self.label_16.setObjectName(u"label_16")
+        self.label_16.setGeometry(QRect(50, 10, 430, 50))
+        self.label_16.setFont(font2)
+        self.label_16.setAlignment(Qt.AlignmentFlag.AlignLeading|Qt.AlignmentFlag.AlignLeft|Qt.AlignmentFlag.AlignTop)
+        self.sw_nav_bar.addWidget(self.SoO)
+        self.spare = QWidget()
+        self.spare.setObjectName(u"spare")
+        self.label_13 = QLabel(self.spare)
+        self.label_13.setObjectName(u"label_13")
+        self.label_13.setGeometry(QRect(120, 120, 341, 161))
+        self.label_13.setFont(font3)
+        self.label_17 = QLabel(self.spare)
+        self.label_17.setObjectName(u"label_17")
+        self.label_17.setGeometry(QRect(50, 10, 430, 50))
+        self.label_17.setFont(font2)
+        self.label_17.setAlignment(Qt.AlignmentFlag.AlignLeading|Qt.AlignmentFlag.AlignLeft|Qt.AlignmentFlag.AlignTop)
+        self.sw_nav_bar.addWidget(self.spare)
         MainWindow.setCentralWidget(self.centralwidget)
+        self.widget.raise_()
+        self.pb_Schnittstellen.raise_()
+        self.pb_SoO.raise_()
+        self.pb_spare.raise_()
+        self.sw_nav_bar.raise_()
 
         self.retranslateUi(MainWindow)
 
-        self.sw_test.setCurrentIndex(0)
+        self.sw_nav_bar.setCurrentIndex(0)
 
 
         QMetaObject.connectSlotsByName(MainWindow)
@@ -70,29 +233,40 @@ class Ui_MainWindow(object):
         self.pb_Schnittstellen.setText(QCoreApplication.translate("MainWindow", u"Schnittstellen", None))
         self.pb_SoO.setText(QCoreApplication.translate("MainWindow", u"SoO", None))
         self.pb_spare.setText(QCoreApplication.translate("MainWindow", u"spare", None))
-        self.textEdit.setHtml(QCoreApplication.translate("MainWindow", u"<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
-"<html><head><meta name=\"qrichtext\" content=\"1\" /><meta charset=\"utf-8\" /><style type=\"text/css\">\n"
-"p, li { white-space: pre-wrap; }\n"
-"hr { height: 1px; border-width: 0; }\n"
-"li.unchecked::marker { content: \"\\2610\"; }\n"
-"li.checked::marker { content: \"\\2612\"; }\n"
-"</style></head><body style=\" font-family:'Segoe UI'; font-size:9pt; font-weight:400; font-style:normal;\">\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Inputs Schnittstellen</p></body></html>", None))
-        self.textEdit_2.setHtml(QCoreApplication.translate("MainWindow", u"<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
-"<html><head><meta name=\"qrichtext\" content=\"1\" /><meta charset=\"utf-8\" /><style type=\"text/css\">\n"
-"p, li { white-space: pre-wrap; }\n"
-"hr { height: 1px; border-width: 0; }\n"
-"li.unchecked::marker { content: \"\\2610\"; }\n"
-"li.checked::marker { content: \"\\2612\"; }\n"
-"</style></head><body style=\" font-family:'Segoe UI'; font-size:9pt; font-weight:400; font-style:normal;\">\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Inputs SoO</p></body></html>", None))
-        self.textEdit_3.setHtml(QCoreApplication.translate("MainWindow", u"<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
-"<html><head><meta name=\"qrichtext\" content=\"1\" /><meta charset=\"utf-8\" /><style type=\"text/css\">\n"
-"p, li { white-space: pre-wrap; }\n"
-"hr { height: 1px; border-width: 0; }\n"
-"li.unchecked::marker { content: \"\\2610\"; }\n"
-"li.checked::marker { content: \"\\2612\"; }\n"
-"</style></head><body style=\" font-family:'Segoe UI'; font-size:9pt; font-weight:400; font-style:normal;\">\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Inputs spare</p></body></html>", None))
+        self.label.setText(QCoreApplication.translate("MainWindow", u"  LIEBHERR", None))
+        self.label1.setText(QCoreApplication.translate("MainWindow", u"Projekt", None))
+        self.label_2.setText(QCoreApplication.translate("MainWindow", u"Kunde", None))
+        self.label_3.setText(QCoreApplication.translate("MainWindow", u"Kontakt", None))
+        self.label_4.setText(QCoreApplication.translate("MainWindow", u"Anlage", None))
+        self.label_5.setText(QCoreApplication.translate("MainWindow", u"Hardware", None))
+        self.label_6.setText(QCoreApplication.translate("MainWindow", u"Opt. 24-poliger Stecker", None))
+        self.le_fn_number.setPlaceholderText(QCoreApplication.translate("MainWindow", u"FN-Nummer", None))
+        self.le_customer.setPlaceholderText(QCoreApplication.translate("MainWindow", u"z.B. Mercedes-Benz AG", None))
+        self.le_firstname.setPlaceholderText(QCoreApplication.translate("MainWindow", u"Vorname", None))
+        self.le_lastname.setPlaceholderText(QCoreApplication.translate("MainWindow", u"Nachname", None))
+        self.cob_typ.setItemText(0, QCoreApplication.translate("MainWindow", u"Ladeportal", None))
+        self.cob_typ.setItemText(1, QCoreApplication.translate("MainWindow", u"PHS", None))
+        self.cob_typ.setItemText(2, QCoreApplication.translate("MainWindow", u"Roboter", None))
+
+        self.cob_hardware.setItemText(0, QCoreApplication.translate("MainWindow", u"Profinet", None))
+        self.cob_hardware.setItemText(1, QCoreApplication.translate("MainWindow", u"Profibus", None))
+        self.cob_hardware.setItemText(2, QCoreApplication.translate("MainWindow", u"Hardwired", None))
+
+        self.label_11.setText(QCoreApplication.translate("MainWindow", u"L\u00e4nge Interlock", None))
+        self.cob_interlock_length.setItemText(0, QCoreApplication.translate("MainWindow", u"32 Byte", None))
+        self.cob_interlock_length.setItemText(1, QCoreApplication.translate("MainWindow", u"64 Byte", None))
+        self.cob_interlock_length.setItemText(2, QCoreApplication.translate("MainWindow", u"128 Byte", None))
+
+        self.cb_24_pins.setText("")
+        self.pb_start.setText(QCoreApplication.translate("MainWindow", u"Start", None))
+        self.label_10.setText(QCoreApplication.translate("MainWindow", u"Sprache", None))
+        self.cob_language.setItemText(0, QCoreApplication.translate("MainWindow", u"Deutsch", None))
+
+        self.le_email.setPlaceholderText(QCoreApplication.translate("MainWindow", u"E-Mail", None))
+        self.label_15.setText(QCoreApplication.translate("MainWindow", u"Schnittstellenbeschreibung", None))
+        self.label_12.setText(QCoreApplication.translate("MainWindow", u"not programmed yet", None))
+        self.label_16.setText(QCoreApplication.translate("MainWindow", u"Systembeschreibung", None))
+        self.label_13.setText(QCoreApplication.translate("MainWindow", u"not programmed yet", None))
+        self.label_17.setText(QCoreApplication.translate("MainWindow", u"spare", None))
     # retranslateUi
 
